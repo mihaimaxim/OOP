7f3235199d5d3ede361221c96774893fa7c2162b
Mihai Maxim <mihaimaxim90@icloud.com>
2020-07-26 14:50:27 +0300

    class split

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,14 +1,9 @@
 class Product {
-   // title = "DEFAULT";
-   // imageURL;
-   // price;
-   // description;
-
-   constructor(title, imageURL, price, description) {
+   constructor(title, image, price, desc) {
       this.title = title;
-      this.imageURL = imageURL;
+      this.imageURL = image;
       this.price = price;
-      this.description = description;
+      this.description = desc;
    }
 }
 
@@ -22,7 +17,7 @@ class ProductItem {
       prodEl.className = 'product-item';
       prodEl.innerHTML = `
         <div>
-          <img src="${this.product.imageUrl}" alt="${this.product.title}" >
+          <img src="${this.product.imageURL}" alt="${this.product.title}">
           <div class="product-item__content">
             <h2>${this.product.title}</h2>
             <h3>\$${this.product.price}</h3>
@@ -39,15 +34,15 @@ class ProductList {
    products = [
       new Product(
          'a pillow',
-         'https://www.piatafinanciara.ro/wp-content/uploads/2020/04/apple.jpeg',
+         'https://www.shopmarriott.com/images/products/v2/xlrg/Marriott-The-Marriott-Pillow-MAR-108-L_xlrg.jpg',
          99,
          'a soft pillow'
       ),
       new Product(
          'a melon',
-         'https://iberianorigin.com/54-large_default/black.jpg',
+         'https://upload.wikimedia.org/wikipedia/commons/2/28/Cantaloupes.jpg',
          9,
-         'a dark melon'
+         'a golden melon'
       ),
    ];
 
