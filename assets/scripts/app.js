144d9d50aa1c970b8cbdca296314ee260f12ae59
Mihai Maxim <mihaimaxim90@icloud.com>
2020-07-26 13:53:54 +0300

    before

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,51 +4,67 @@ class Product {
    // price;
    // description;
 
-   constructor(title, imageURL, price, description) {
+   constructor(title, image, price, description) {
       this.title = title;
-      this.imageURL = imageURL;
+      this.imageURL = image;
       this.price = price;
       this.description = description;
    }
 }
 
-const productList = {
-   products: [
-      new Product(
-         "a pillow",
-         "https://www.shopmarriott.com/images/products/v2/xlrg/Marriott-The-Marriott-Pillow-MAR-108-L_xlrg.jpg",
-         99,
-         "a soft pillow"
-      ),
-      new Product(
-         "a melon",
-         "https://iberianorigin.com/54-large_default/black.jpg",
-         9,
-         "a dark melon"
-      ),
-   ],
+class ProductItem {
+   constructor(product) {
+      this.product = product;
+   }
+
    render() {
-      const renderHook = document.getElementById("app");
-      const prodList = document.createElement("ul");
-      prodList.className = "product-list";
-      for (const prod of this.products) {
-         const prodItem = document.createElement("li");
-         prodItem.className = "product-item";
-         prodItem.innerHTML = `
-            <div>
-               <img src="${prod.imageURL}" alt="${prod.title}" />
-               <div class="product-item__content">
-                  <h2>${prod.title}</h2>
-                  <h3>\$${prod.price}</h3>
-                  <p>${prod.description}</p>
-                     <button>add to cart</button>
-               </div>
+      const prodEl = document.createElement('li');
+      prodEl.className = 'product-item';
+      prodEl.innerHTML = `
+         <div>
+            <img src="${this.product.imageURL}" alt="${this.product.title}" />
+            <div class="product-item__content">
+      	      <h2>${this.product.title}</h2>
+      	      <h3>\$${this.product.price}</h3>
+               <p>${this.product.description}</p>
+               <button>add to cart</button>
             </div>
-         `;
-         prodList.append(prodItem);
+         </div>
+		`;
+      return prodEl;
+   }
+}
+
+class ProductList {
+   products = [
+      new Product(
+         'a pillow',
+         'https://www.shopmarriott.com/images/products/v2/xlrg/Marriott-The-Marriott-Pillow-MAR-108-L_xlrg.jpg',
+         99,
+         'a soft pillow'
+      ),
+      new Product(
+         'a melon',
+         'https://iberianorigin.com/54-large_default/black.jpg',
+         9,
+         'a dark melon'
+      ),
+   ];
+
+   constructor() {}
+
+   render() {
+      const renderHook = document.getElementById('app');
+      const prodList = document.createElement('ul');
+      prodList.className = 'product-list';
+      for (const prod of this.products) {
+         const ProductItem = new Product();
+         const prodEl = ProductItem.render();
+         prodList.append(prodEl);
       }
       renderHook.append(prodList);
-   },
-};
+   }
+}
 
+const productList = new ProductList();
 productList.render();
